0a766e477315010c906a20e5bf6fdcf6baebc8f5
maiyama <myam0118@gmail.com>
2022-03-06 09:40:50 +0900

    Add intent handling

---
+++ IssueGazer/IssueGazerIntent/IntentHandler.swift
@@ -0,0 +1,20 @@
+//
+//  IntentHandler.swift
+//  IssueGazerIntent
+//
+//  Created by maiyama on 2022/03/05.
+//
+
+import Intents
+
+class IntentHandler: INExtension {}
+
+extension IntentHandler: IssuesSearchConfigurationIntentHandling {
+    func provideQueryOptionsCollection(for intent: IssuesSearchConfigurationIntent) async throws -> INObjectCollection<Query> {
+        let query1 = Query(identifier: "query1", display: "My Issues")
+        query1.text = "query 1"
+        let query2 = Query(identifier: "query2", display: "Assigned Issues")
+        query2.text = "query 2"
+        return INObjectCollection(items: [query1, query2])
+    }
+}
